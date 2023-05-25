80ee6fd9c1faa28ac552b809892181f805ec05c8
gabriel.faria-at-325485480401 <A/PpARTh6b6kxlSwx98GMe0TxlahiISbPK8z2URXZjY=>
2023-05-25 12:08:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Header} from './components/Header';
+import {Post} from './Post'
+
+import './global.css';
+
+export function App(){
+  return(
+    <div>
+      <Header />
+
+      <Post
+        author="Diego Fontes"
+        content= 
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from '. /Header.modules.css'
+
+export function Header() {
+    return(
+        <header className={styles.header}>
+            <stong>Ignite Feed</stong>
+            
+        </header>
+    );
+}
